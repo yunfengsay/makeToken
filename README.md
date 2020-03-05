eeddfaa2b3073845bc2b9705addd0c0414b60b31
yunfengzhang.zyf <yunfengzhang.zyf@alibaba-inc.com>
2020-03-05 11:37:44 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # makeToken
 分词工具
+useages:
+```
+maketoken ./xxx.txt ./xxx.tokened.txt
+```
---
+++ makeToken.py
@@ -0,0 +1,22 @@
+import jieba
+import chardet
+import sys
+
+
+def parse_args():
+    print(sys.argv)
+    return sys.argv[1], sys.argv[2]
+
+
+def main():
+    file, target = parse_args()
+    charType = chardet.detect(open(file, 'rb').read())
+    print(charType)
+    with open(file,'r',errors='ignore', encoding=charType.get('encoding', 'utf-8')) as fp:
+        lines = fp.readlines()
+        for line in lines:
+            seg_list = jieba.cut(line)
+            with open(target, 'a+', encoding='utf-8') as ff:
+               	ff.write(' '.join(seg_list))
+    print('ok')
+main()
